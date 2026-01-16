d3e5ee6366a2de5877477b877b826f6250573273
Muneeb2721 <muneebnadeem1870@gmail.com>
2026-01-16 20:50:52 +0500

    feat: implement dynamic Our Core Services section with slider animation
    - Created Our Core Services component with data fetched from TS data file
    - Integrated component into About page
    - Added Framer Motion for smooth sliding animations
    - Enabled slider behavior and updated services data to support slide-based content
    - Downloaded and integrated respective service icons
    - Ensured full responsiveness across all screen sizes

---
+++ components/sections/about/Services/page.tsx
@@ -0,0 +1,11 @@
+import ServiceCard from "./ServiceCard"
+
+const Services = () => {
+    return (
+        <div className="">
+            <ServiceCard />
+        </div>
+    )
+}
+
+export default Services
